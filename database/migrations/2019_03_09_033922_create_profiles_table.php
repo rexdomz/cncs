28f9a05e905e37310771133ee047dcc92b9f0cf4
rexdomz <dominguezrex@gmail.com>
2019-03-09 11:57:44 +0800

    add two tables

---
+++ database/migrations/2019_03_09_033922_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('full_name', 100);
+            $table->string('address', 100);
+            $table->string('area', 10);
+            $table->double('loan', 15, 5);
+            $table->integer('interest');
+            $table->integer('term');
+            $table->string('contact', 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
